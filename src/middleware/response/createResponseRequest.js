37b3fab3651ec60582527acf792ba89a53f67ec7
Jop <jop.huijbers2001@gmail.com>
2022-05-16 12:46:40 +0200

    Check if answers array exists when saving response

--- src/middleware/response/createResponseRequest.js
+++ src/middleware/response/createResponseRequest.js
@@ -13,6 +13,9 @@ module.exports = function createResponseRequest(req, res, next) {
 
     req.survey.questions.forEach(question => {
         if (question.required) {
+            if (!req.body.answers) {
+                return res.status(400).json({message: 'Survey response must provide an answer to all required questions'})
+            }
             const answer = req.body.answers.find(answer => answer.question._id == question._id)
             if (!answer) {
                 return res.status(400).json({message: 'Survey response must provide an answer to all required questions'})
